bc4d78b5204041fee6529e6892c079a8517d4227
Mpho Mphego <mpho112@gmail.com>
2020-09-01 09:27:36 +0200

    Updated  ../__init__.py
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- pyvino_utils/__init__.py
+++ pyvino_utils/__init__.py
@@ -5,7 +5,4 @@
 __author__ = """Mpho Mphego"""
 __email__ = "mpho112@gmail.com"
 
-from .input_handler.input_feeder import InputFeeder
-from .models.facial import *
-from .models.openvino_base.base_model import Base
-from .opencv_utils import cv_utils
+from . import *
