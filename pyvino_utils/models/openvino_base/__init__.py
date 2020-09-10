a3750fa65522861cd4fa6c2bdc8e4c62ba6ad1c8
Mpho Mphego <mpho112@gmail.com>
2020-09-10 08:09:23 +0200

    Skating on thin ice, literally.
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- pyvino_utils/models/openvino_base/__init__.py
+++ pyvino_utils/models/openvino_base/__init__.py
@@ -2,6 +2,11 @@
 import os
 import pkgutil
 
-from . import base_model
-
 __all__ = [module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)])]
+
+# Hackery!!!
+# FYI -> Skating on thin ice, literally.
+# Be careful with exec (see: https://lucumr.pocoo.org/2011/2/1/exec-in-python/)
+
+for _module in __all__:
+    exec(f"from . import {_module}")
