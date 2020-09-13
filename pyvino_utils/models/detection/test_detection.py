4d909111cadaf19ab1f91b8e567caa4f601afc9d
Mpho Mphego <mpho112@gmail.com>
2020-09-13 09:47:57 +0200

    Updated
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

---
+++ pyvino_utils/models/detection/test_detection.py
@@ -0,0 +1,28 @@
+import cv2  # noqa: F401
+import numpy as np  # noqa: F401
+
+from ..openvino_base.base_model import Base
+
+
+class TextDetection(Base):
+    """Class for the Text Detection Model."""
+
+    def __init__(
+        self,
+        model_name,
+        source_width=None,
+        source_height=None,
+        device="CPU",
+        threshold=0.60,
+        extensions=None,
+    ):
+        super().__init__(
+            model_name, source_width, source_height, device, threshold, extensions,
+        )
+
+    def preprocess_output(self, inference_results, image, show_bbox):
+        raise NotImplementedError("Method not implemented!")
+
+    @staticmethod
+    def draw_output(coords, image):
+        raise NotImplementedError("Method not implemented!")
---
+++ pyvino_utils/models/recognition/text_recognition.py
@@ -0,0 +1,31 @@
+import time
+
+import cv2
+import numpy as np
+
+from ..openvino_base.base_model import Base
+
+
+class TextRecognition(Base):
+    """Class for the Text Recognition Model."""
+
+    def __init__(
+        self,
+        model_name,
+        source_width=None,
+        source_height=None,
+        device="CPU",
+        threshold=0.60,
+        extensions=None,
+    ):
+        super().__init__(
+            model_name, source_width, source_height, device, threshold, extensions,
+        )
+
+    def preprocess_output(self, inference_results, image, show_bbox, **kwargs):
+        results = {}
+        return results, image
+
+    @staticmethod
+    def draw_output(results, image, **kwargs):
+        pass
